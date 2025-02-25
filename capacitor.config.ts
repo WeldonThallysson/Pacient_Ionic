09b60955921078eb2f8447a46bcd8dbc49bf5a60
Weldon Thallysson <weldonthallysson.dev@gmail.com>
2025-02-25 18:14:55 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Pacients ',
+  webDir: 'dist'
+};
+
+export default config;
